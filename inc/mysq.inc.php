54b7dd82f96617aa28104c7790ffa057ec63e28f
Joshua Palmer <joshua.after@gmail.com>
2020-12-02 19:57:05 -0800

    add query_db -- runs fully prepared SQL queries

--- inc/mysq.inc.php
+++ inc/mysq.inc.php
@@ -27,4 +27,59 @@ function db_close( $cnx ) {
     if(  $cnx  )
         mysqli_close( $cnx );
 }
+
+//==========================================================================
+// db_query
+//   Run a fully prepared mysqli query from start to finish. 
+//   Returns false if no query results. Returns results otherwise.
+//   result_type is optional and returns associative array if not assigned.
+//   Other result_type options are MYSQLI_NUM and MYSQLI_BOTH.
+//==========================================================================
+function db_query( $query_string, $param_array, $result_type = MYSQLI_ASSOC ) {
+    // validate input
+    if( strlen($query_string) == 0 ) {
+        LOG_ERROR( "mysqli query attempted with empty query string" );
+        die( "An error occurred. Please contact the site administrator if this problem persists." );
+    }
+
+    // open connection to db
+    $cnx = db_open();
+
+    // prepare query statment
+    $types = str_repeat( "s", count($param_array) );
+    $stmt = mysqli_prepare( $cnx, $query_string );
+    if( !$stmt ) {
+        LOG_ERROR( "Failed to build prepared statment while running mysqli query. Check for syntax errors in query string." );
+        die( "An error occurred. Please contact the site administrator if this problem persists." );
+    }
+    mysqli_stmt_bind_param( $stmt, $types, ...$param_array );
+
+    // execute query
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result( $stmt );
+
+    // validate query results
+    $return_value = false;
+
+    // If this was a successful insert/update/delete
+    // query, result == false but errno == 0.
+    // In that case, return true to show that it succeeded.
+    if( !$result and mysqli_errno($cnx) === 0) {
+        $return_value = true;
+    }
+    // Otherwise, this was a select statement.
+    // Return the results.
+    else if( $result ) {
+        $return_value = mysqli_fetch_all( $result, $result_type );
+    }
+    else {
+        LOG_ERROR( "mysqli query failed with errorcode: " . mysqli_errno($cnx) );
+        die( "An error occurred. Please contact the site administrator if this problem persists." );
+    }
+
+    // close connection to db
+    db_close( $cnx );
+
+    return $return_value;
+}
 ?>
