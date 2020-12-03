1b9acb8e15283050619e66d7b004adec46abd6b3
Joshua Palmer <joshua.after@gmail.com>
2020-12-02 19:02:17 -0800

    Utility script for logging errors and debug.

---
+++ inc/logging.inc.php
@@ -0,0 +1,32 @@
+<?php
+define( "LOGS_PATH", $_SERVER["DOCUMENT_ROOT"] . "/luv/logs/" );
+
+// Log messages are sent to their respective type of file (error or debug).
+// A new log file for each type is generated daily, named like:
+// "2020-11-30_error".
+
+//==========================================================================
+// LOG_ERROR
+//   Logging for critical errors for information
+//   that shouldn't be displayed to the user.
+//==========================================================================
+function LOG_ERROR( $message ) {
+    $filename = LOGS_PATH . date("Y-m-d") . "_error";
+    $msg_body = date("H:i:s") . ": " . $message . "\n";
+    $f = fopen($filename, "a") or die();
+    fwrite($f, $msg_body);
+    fclose($f);
+}
+
+//==========================================================================
+// LOG_DEBUG
+//   Logging for development purposes only.
+//==========================================================================
+function LOG_DEBUG( $message ) {
+    $filename = LOGS_PATH . date("Y-m-d") . "_debug";
+    $msg_body = date("H:i:s") . ": " . $message . "\n";
+    $f = fopen($filename, "a") or die();
+    fwrite($f, $msg_body);
+    fclose($f);
+}
+?>
