aff0657ff525bdb1e23c5267d49eb591e5099276
DESKTOP-KD6EEGF\DanMe <mendo263@cougars.csusm.edu>
2020-12-05 15:08:22 -0800

    tset

--- messages.php
+++ messages.php
@@ -102,10 +102,10 @@ landing page for luv dating site
                             ?>
                     </div>
                 
-                    <script>
-                        var lastDiv = document.getElementById("message-container-div").lastChild();
-                        lastDiv.scrollIntoView();
-                    </script>
+                <script>
+                    document.getElementById("message-container-div").lastChild.scrollIntoView();
+                </script>
+                    
                 <script>
                     var show_sent_message = function( div_color, message_data ) {
                         var outer_div = document.createElement( "DIV" );
