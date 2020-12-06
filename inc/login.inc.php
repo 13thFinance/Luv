7e1a3324d0ea1f4f5362123cd81bdd6f1259fa25
Justin McCarthy <jmmccarthy94@yahoo.com>
2020-12-06 11:51:57 -0800

    fixed double query error

--- inc/login.inc.php
+++ inc/login.inc.php
@@ -40,9 +40,9 @@ function login( $email, $pwd ) {
     // set an auth_token for the member in the db
     $query_string = "update members set auth_token=? where member_id=?";
     $query_params = [$auth_token, $member_id];
-    $result = db_query( $query_string, $query_params );
+    $result_update = db_query( $query_string, $query_params );
 
-    if( $result === false ) {
+    if( $result_update === false ) {
         // JOSH TODO: Change this to redirect back to the create/login page with a similar error.
         LOG_ERROR( "Login error -- failed to update auth_token, member_id='$member_id'" );
         die( "PLACEHOLDER: Something went wrong while logging in." );
@@ -59,7 +59,7 @@ function login( $email, $pwd ) {
     setcookie( "member_id", $member_id, $expiry_time, "/luv");
 
     // redirect to user's profile or admin page if admin
-    if( $result[0]["is_admin"] == 1 ){
+    if( $result[0]["is_admin"] == "1" ){
         header( "location: ../admin-page.php" );
     }
     else{
