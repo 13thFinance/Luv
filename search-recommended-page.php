47f2fcaa42460d48011cd516b689320f910b6e5f
DESKTOP-KD6EEGF\DanMe <mendo263@cougars.csusm.edu>
2020-12-05 19:46:40 -0800

    test

--- search-recommended-page.php
+++ search-recommended-page.php
@@ -93,7 +93,7 @@ else
                             "<input type='hidden' name='target_id' value='" + member_id + "'/> " +
                         "</form> " + 
                     "   <form class='search-profile-form' action='message.php' method='post'> " +
-                    "        <input type='submit' class='search-profile-button search-profile-message-button' value='Message'/> " +
+                    "        <input type='submit' class='search-profile-button search-profile-view-button' value='Message'/> " +
                     "        <input type='hidden' name='target_id' value='" + member_id + "'/> " +
                         "</form> " +
                     "</div>  ";
