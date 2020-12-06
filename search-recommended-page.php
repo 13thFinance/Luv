b6a470bb92e8b00c2a3d36e635525a6fb5713663
DESKTOP-KD6EEGF\DanMe <mendo263@cougars.csusm.edu>
2020-12-05 18:04:40 -0800

    test

--- search-recommended-page.php
+++ search-recommended-page.php
@@ -65,11 +65,12 @@ else
             
             // PLACEHOLDER CONSOLE OUTPUT
             
+            /*
             $name = member.name;
             $aboutMe = member.about_me;
             $picture = member.picture;
             $rating = member.rating;
-            
+            */
             
             
         });
